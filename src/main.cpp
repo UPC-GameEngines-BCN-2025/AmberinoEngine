6340a6135cec3e22a3372cc0f2b177bc645dd755
sofialiles55 <sofia.liles.5@gmail.com>
2025-09-25 19:52:40 +0200

    trying to make a menu

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,34 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <SDL3/SDL.h>
+#include <imgui_impl_opengl3.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui.h>
+#include <glad/glad.h>
 
 int main()
 {
 	//variables
 	bool running = true;
 	SDL_Event event;
+	bool demoRunning = true;
 
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+	SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
 
 	SDL_Window* window = SDL_CreateWindow("Welcome to AmberinoEngine", 600, 600, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(window);
 	
+	SDL_GL_MakeCurrent(window, context);
+	SDL_GL_SetSwapInterval(1); 
+	SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+	SDL_ShowWindow(window);
+	ImGui::CreateContext();
+	ImGui_ImplSDL3_InitForOpenGL(window, context);
+	ImGui_ImplOpenGL3_Init("#version 130");
+
 	while (running)
 	{
 		while (SDL_PollEvent(&event))
@@ -23,6 +41,31 @@ int main()
 				running = false;
 			}
 		}
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplSDL3_NewFrame();
+		ImGui::NewFrame();
+
+		if (demoRunning)
+		{
+			ImGui::ShowDemoWindow(&demoRunning);
+		}
+		ImGui::Render();
+		glViewport(0, 0, 600, 600);
+		glClearColor(1.f,1.f,1.f,1.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		SDL_GL_SwapWindow(window);
 	}
+	
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplSDL3_Shutdown();
+	ImGui::DestroyContext();
+
+	SDL_GL_DestroyContext(context);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
 	return 0;
 }
